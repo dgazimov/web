dd2bb33182abffa3fe3cd3afdb688b79c7dc816d
dgazimov <115115334+dgazimov@users.noreply.github.com>
2022-10-18 06:58:24 +0300

    Add files via upload

--- homework1/script.js
+++ homework1/script.js
@@ -1,39 +1,39 @@
-function plus () {
-    var num1, num2, result;
-    num1 = document.getElementById ('n1').value;
-    num1 = parseInt(num1);
+function calc ()
+{
+    var n1 = parseInt (document.getElementById ('n1').value);
+    var n2 = parseInt (document.getElementById ('n2').value);
+    var oper = document.getElementById ('operators').value;
 
-    num2 = document.getElementById ('n2').value;
-    num2 = parseInt(num2);
-
-    result = num1+num2;
-    document.getElementById('result').innerHTML = result;
+    if (oper === '+')
+    {
+        document.getElementById ('result').value = n1+n2;
+    }
+    if (oper === '-')
+    {
+        document.getElementById ('result').value = n1-n2;
+    }
+    if (oper === '/')
+    {
+        document.getElementById ('result').value = n1/n2;
+    }
+    if (oper === '*')
+    {
+        document.getElementById ('result').value = n1*n2;
+    }
+    
+    let element_window = document.querySelector (".mask"); //получение доступа к окну
+    element_window.classList.remove("hidden"); //убрали класс hidden из списка классов этого окна
 }
 
-function minus () {
-    var num1, num2, result;
-    num1 = document.getElementById ('n1').value;
-    num1 = parseInt(num1);
 
-    num2 = document.getElementById ('n2').value;
-    num2 = parseInt(num2);
-
-    result = num1-num2;
-    document.getElementById('result').innerHTML = result;
-}
 function show () {
-let element_input = document.querySelector(".calculate");
-let data = element_input.value
 
-let element_result = document.querySelector (".result");
-element_result.innerHTML = data
-
- let element_window = document.querySelector (".mask"); //получение доступа к окну
- element_window.classList.remove("hidden"); //убрали класс hidden из списка классов этого окна
-
-}
-
-function hide() {
-    let element_window = document.querySelector(".mask");
-    element_window.classList.add("hidden");
-}
+     let element_window = document.querySelector (".mask"); //получение доступа к окну
+     element_window.classList.remove("hidden"); //убрали класс hidden из списка классов этого окна
+    
+    }
+    
+    function hide() {
+        let element_window = document.querySelector(".mask");
+        element_window.classList.add("hidden");
+    }
