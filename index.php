86a942f983596754d9cea229f6d9d8f9454726de
dgazimov <115115334+dgazimov@users.noreply.github.com>
2022-12-02 11:23:26 +0300

    php calculator
    php calculator

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+
+ $connection = mysqli_connect("localhost", "root", "", "test");
+ if (!$connection) {
+     die ("Связь не установлена: " .mysqli_connect_error());
+ }
+
+ $query = mysqli_query($connection, "SELECT * FROM `resul` ORDER BY `id` DESC limit 7");
+
+ $resul = [];
+ $num1 = [];
+ $num2 = [];
+ $operation = [];
+
+ while ($row = mysqli_fetch_assoc($query)){
+        
+    $resul[] = $row;
+ }
+ 
+ 
+  if(isset($_POST['calculation'])){
+      $num1 = $_POST ['num1'];
+      $num2 = $_POST ['num2'];
+      $operation = $_POST ['op'];
+
+      if ($operation == '+'){
+          $summa = $num1 + $num2;
+          mysqli_query($connection, "INSERT INTO `resul`(`result`, `num1`, `num2`, `op`) VALUES ('$summa','$num1','$num2','$operation')");
+      }else if ($operation == '-'){
+          $summa = $num1 - $num2;
+          mysqli_query($connection, "INSERT INTO `resul`(`result`, `num1`, `num2`, `op`) VALUES ('$summa','$num1','$num2','$operation')");
+      }else if ($operation == '/'){
+          $summa = $num1 / $num2;
+          mysqli_query($connection, "INSERT INTO `resul`(`result`, `num1`, `num2`, `op`) VALUES ('$summa','$num1','$num2','$operation')");
+      }else if ($operation == '*'){
+          $summa = $num1 * $num2;
+          mysqli_query($connection, "INSERT INTO `resul`(`result`, `num1`, `num2`, `op`) VALUES ('$summa','$num1','$num2','$operation')");
+      }
+  }
+?>
+
 <html>
     <head>
     <title>Калькулятор</title>
@@ -19,56 +60,13 @@
             <?php echo $summa; ?>
             </div>
     <?php } ?>
-                     
-</body>
-</html>
 
-
-<?php
-
- $connection = mysqli_connect("localhost", "root", "", "test");
- if (!$connection) {
-     die ("Связь не установлена: " .mysqli_connect_error());
- }
-
- $query = mysqli_query($connection, "SELECT * FROM `resul` ORDER BY `id` DESC limit 7");
-
- $resul = [];
- $num1 = [];
- $num2 = [];
- $operation = [];
-
- while ($row = mysqli_fetch_assoc($query)){
-        
-    $resul[] = $row;
- }
-    
-    if(isset($_POST['calculation'])){
-        $num1 = $_POST ['num1'];
-        $num2 = $_POST ['num2'];
-        $operation = $_POST ['op'];
-
-        if ($operation == '+'){
-            $summa = $num1 + $num2;
-            echo $summa;
-            mysqli_query($connection, "INSERT INTO `resul`(`result`, `num1`, `num2`, `op`) VALUES ('$summa','$num1','$num2','$operation')");
-        }else if ($operation == '-'){
-            $summa = $num1 - $num2;
-            echo $summa;
-            mysqli_query($connection, "INSERT INTO `resul`(`result`, `num1`, `num2`, `op`) VALUES ('$summa','$num1','$num2','$operation')");
-        }else if ($operation == '/'){
-            $summa = $num1 / $num2;
-            echo $summa;
-            mysqli_query($connection, "INSERT INTO `resul`(`result`, `num1`, `num2`, `op`) VALUES ('$summa','$num1','$num2','$operation')");
-        }else if ($operation == '*'){
-            $summa = $num1 * $num2;
-            echo $summa;
-            mysqli_query($connection, "INSERT INTO `resul`(`result`, `num1`, `num2`, `op`) VALUES ('$summa','$num1','$num2','$operation')");
-        }
-    }
-    foreach ($resul as $result) { ?>
+    <?php foreach ($resul as $result) { ?>
         <div>
             <span><?php echo $result["id"]; ?></span> 
             <?php echo $result ["result"]; ?>
     </div>
     <?php }?>
+                     
+</body>
+</html>
